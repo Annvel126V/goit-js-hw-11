1aa9b04881e9c8265f2a1db23286d0f1aa131815
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-07-28 00:22:13 +0200

    fix gallery

--- src/js/pixabay.api.js
+++ src/js/pixabay.api.js
@@ -1,24 +1,27 @@
-
+    // Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+// Описаний у документації
 
     export  function searchImages(query) { 
-    const URL = "https://pixabay.com/api"; 
+    const URL = "https://pixabay.com/api/"; 
     const API_KEY = "45110935-91aa782eb5f23a60393d7a38c";
     
-      
-    
-      return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`).then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      }).catch((error) => {
-        iziToast.error({
-          position: "topRight",
-          message: `${error}`,
-           
-        });
-      });
-      
-  }
+     const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  return fetch(`${URL}?${searchParams}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
 
 
